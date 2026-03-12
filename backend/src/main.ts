192f98b9ec9fef09948f0b41a9b9ebb698455924
chechojgb <100326731+chechojgb@users.noreply.github.com>
2026-03-12 08:52:44 -0500

    fix: add SSL config for Neon in production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,38 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet({
-    contentSecurityPolicy: false,
-  }));
+  app.use(helmet());
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://prescriptions-app-ruddy.vercel.app'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://prescriptions-app-ruddy.vercel.app'],
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
-    forbidNonWhitelisted: true,
   }));
-
 
   const config = new DocumentBuilder()
     .setTitle('Prescriptions API')
-    .setDescription('API for system to prescription medical')
+    .setDescription('API for system to prescription medical ')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
@@ -40,15 +29,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.PORT || 3000;
-  
-  try {
-    await app.listen(port, '0.0.0.0');
-    logger.log(`application is running on: http://0.0.0.0:${port}`);
-    logger.log(`Documentación disponible en: http://0.0.0.0:${port}/docs`);
-  } catch (error) {
-    logger.error(`Error starting server: ${error.message}`);
-  }
+  await app.listen(process.env.APP_PORT || 3000);
 }
 
 bootstrap();
